bc50421ab9f6676b919777ffeb20ad56b60ca2ec
waltiu <10470167@qq.com>
2019-08-16 17:52:51 +0800

    地图

--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,14 +1,34 @@
 <template>
-	<div>
-		adsa
-	</div>
+  <div>
+    <div>保护</div>
+    <div id="map" class="container"></div>
+  </div>
 </template>
-
 <script>
-	export default{
-	
-	}
+import 'maptalks/dist/maptalks.css';
+import * as maptalks from 'maptalks';
+import axios from 'axios'
+export default {
+  mounted () {
+    this.$nextTick(() => {
+      const map = new maptalks.Map('map', {
+        center: [-0.113049, 51.498568],
+        zoom: 14,
+        baseLayer: new maptalks.TileLayer('base', {
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+          attribution: '&copy; <a href="http://www.osm.org" target="_blank">OpenStreetMap</a> contributors'
+        }),
+      });
+      // console.log('map: ', map);
+    });
+  },
+};
 </script>
 
-<style>
+<style >
+.container {
+  width: 100%;
+  height: 80vh;
+}
 </style>
