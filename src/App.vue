018b5dabc0def71f22772cfd9cb3a1257538b18a
waltiu <10470167@qq.com>
2019-08-20 17:58:54 +0800

    登陆美化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
- 
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
-	*{
-		margin: 0px;
-		padding: 0px;
-	}
+<style scope>
+* {
+  margin: 0px;
+  padding: 0px;
+}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,70 +1,110 @@
 <template>
-	<div class="login">
-		<el-form :model="loginForm"   ref="ruleForm" label-width="100px" >
-			<el-form-item label="用户名" prop="pass">
-				<el-input  v-model="loginForm.name" ></el-input>
-			</el-form-item>
-			<el-form-item label="密码">
-				<el-input type="password" v-model="loginForm.password" ></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="submitLoginForm('ruleForm')">登陆</el-button>
-				<el-button @click="registForm('ruleForm')">注册</el-button>
-			</el-form-item>
-		</el-form>
-
-	</div>
+  <div
+    class="loginItem"
+    v-loading="loginLoading"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)"
+  >
+    <div class="login">
+      <el-form :model="loginForm" ref="ruleForm" label-width="100px">
+        <el-form-item>
+          <el-button type="primary" size="small">用户名</el-button>
+          <el-input v-model="loginForm.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="small">密&nbsp;&nbsp;&nbsp;&nbsp;码</el-button>
+          <el-input type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            class="loginButton"
+            type="success"
+            icon="el-icon-check"
+            @click="submitLoginForm('ruleForm')"
+            circle
+          ></el-button>
+          <!-- <el-button type="primary" @click="submitLoginForm('ruleForm')">登陆</el-button>
+          <el-button @click="registForm('ruleForm')">注册</el-button>-->
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
-	import axios from 'axios'
-	export default {
-		data(){
-			return {
-				loginForm:{
-					name:'',
-					password:''
-				},
-				personForm:{},
-				personalData:[],
-				data:{}
-				
-			}
-		},
-		methods:{
-			submitLoginForm(){
-				let len=this.personForm.length
-				for ( var i=0;i<len;i++) {
-				if (this.personForm[i].username===this.loginForm.name&&
-				this.personForm[i].password===this.loginForm.password)
-				{
-					sessionStorage.setItem('permission',JSON.stringify(this.personForm[i].permission));
-					this.personalData.push(this.personForm[i])
-					this.$message({
-					          message: '恭喜你，登陆成功',
-					          type: 'success'})
-					this.$router.push('/map')
-					console.log(this.personForm[i].permission)
-					 var data2 = JSON.parse(sessionStorage.getItem('permission'))
-					console.log(data2)
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      loginForm: {
+        name: '',
+        password: ''
+      },
+      personForm: {},
+      personalData: [],
+      data: {},
+      loginLoading: false
 
-				
-				}
-				}
-				}
-			,
-			registForm(){
-				console.log('a')
-			},
-			getLoginForm(){
-				axios.get('/json/person.json')
-				.then((res) => {this.personForm=res.data.personalData})
-				}
-			
-		},
-		mounted() {
-			this.getLoginForm()
-		},
-	}
+    }
+  },
+  methods: {
+    submitLoginForm () {
+      this.loginLoading = true
+      let len = this.personForm.length
+      for (var i = 0; i < len; i++) {
+        if (this.personForm[i].username === this.loginForm.name &&
+          this.personForm[i].password === this.loginForm.password) {
+          sessionStorage.setItem('permission', JSON.stringify(this.personForm[i].permission));
+          this.personalData.push(this.personForm[i])
+          setTimeout(() => {
+            // this.loginLoading = true
+            console.log('1') //此处修改data中的参数时无效
+          }, 5000);
+          this.$message({
+            message: '恭喜你，登陆成功',
+            type: 'success'          })
+          this.$router.push('/map')
+          console.log(this.personForm[i].permission)
+          var data2 = JSON.parse(sessionStorage.getItem('permission'))
+          console.log(data2)
+
+
+        }
+      }
+    }
+    ,
+    registForm () {
+      console.log('a')
+    },
+    getLoginForm () {
+      axios.get('/json/person.json')
+        .then((res) => { this.personForm = res.data.personalData })
+    }
+
+  },
+  mounted () {
+    this.getLoginForm()
+  },
+}
 
 </script>
+<style scoped>
+.loginItem {
+  width: 100%;
+  height: 100%;
+  background-size: 100% 100%;
+  position: fixed;
+  background-image: url(/img/loginBck.jpg);
+}
+.login {
+  position: absolute;
+  width: 30%;
+  height: 10%;
+  margin-top: 15%;
+}
+.loginButton {
+  position: relative;
+  left: 200px;
+}
+</style>
