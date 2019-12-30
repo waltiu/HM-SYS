87a22eb5b4433eacc94171ec6397ab6745dca2be
waltiu <10470167@qq.com>
2019-12-30 09:27:24 +0800

    http

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import '../src/public/index'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'vue2-animate/dist/vue2-animate.min.css'
 import './config/tableConfig/index'
+import './tools/http'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
