2a4117ec4042a1478cac7cb7f0b814b93ba11da0
waltiu <10470167@qq.com>
2019-12-30 09:32:52 +0800

    update

---
+++ src/tools/http.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+const http = axios.create()
+http.interceptors.request.use((config) => {
+  config.headers['x-auth-token'] = 'TL15922232724'
+  return config
+})
+http.interceptors.response.use(response => {
+  return response
+}, error => {
+  console.log(error)
+})
+Vue.prototype.$http = http
